cc51a474454a1fcf0037e080c2d79d793c2da273
Jlinkify <116754620+Jlinkify@users.noreply.github.com>
2023-02-16 16:30:20 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
- new kursor({
-            type: 1,
-            removeDefaultCursor: true
- })
+    const cursorAnimation = document.querySelector(".cursor");
+    const cursors = document.querySelectorAll(".cursor");
+
+    document.addEventListener("click", (e) => {
+      let x = e.clientX;
+      let y = e.clientY;
+
+      cursorAnimation.style.top = y + "px";
+      cursorAnimation.style.left = x + "px";
+
+      cursors.forEach((cursor) => {
+        cursor.classList.add("active");
+
+        function removeCursorActive(){
+          cursor.classList.remove("active");
+        }
+
+        setTimeout(removeCursorActive, 1000);
+      });
+      
+      let cursorClone = cursorAnimation.cloneNode(true);
+      document.querySelector("body").appendChild(cursorClone);
+
+      setTimeout(() => {
+        cursorClone.remove();
+      }, 1000);
+    });
 
 document.getElementById("cards").onmousemove = e => {
   for(const card of document.getElementsByClassName("card")) {
